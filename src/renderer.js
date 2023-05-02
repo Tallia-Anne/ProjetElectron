606d7cbabb27b539f84223819e751040195f3896
Tallia-Anne <erinawambiekele@gmail.com>
2023-05-02 12:26:27 +0200

    modification: renderer, index.html

--- src/renderer.js
+++ src/renderer.js
@@ -1,5 +1,6 @@
 var password=document.getElementById("generator");
     function genPassword() {
+    console.log('ok')
    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
    var passwordLength = 12;
    var password = "";
@@ -7,10 +8,11 @@ var password=document.getElementById("generator");
       var randomNumber = Math.floor(Math.random() * chars.length);
       password += chars.substring(randomNumber, randomNumber +1);
     }
-        document.getElementById("generator").value = password;
+        document.getElementById("password").value = password;
     }
   function copyPassword() {
-  var copyText = document.getElementById("generator");
+   console.log('ok')
+  var copyText = document.getElementById("password");
   copyText.select();
   copyText.setSelectionRange(0, 999);
   }
