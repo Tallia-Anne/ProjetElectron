371025d8b9464d8cd11d07c550d978913b809b68
Tallia-Anne <erinawambiekele@gmail.com>
2023-05-02 12:58:36 +0200

    preload

--- src/preload.js
+++ src/preload.js
@@ -1,2 +1,8 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const {contextBridge, ipcRenderer} = require('electron');
+
+
+contextBridge.exposeInMainWorld('API' , {
+secondWindow: () => ipcRenderer.send('second-window')
+})
